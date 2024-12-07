87f4e10e577da18bc6a91a73618982081c5be0f9
paublancafortguardiola <137887899+paublancafortguardiola@users.noreply.github.com>
2024-12-07 12:17:44 +0100

    Delete Blade_Element_Momentum_Theory directory

--- Blade_Element_Momentum_Theory/getNodes.m
+++
@@ -1,8 +0,0 @@
-function [nodes, dy] = getNodes(y0,y,n)
-    % returns a vector with the position of the centered nodes of each element given the start position, the end position,
-    % and the number of elements
-    dy = (y-y0)/n;
-    nodes = y0:dy:y;
-    nodes = nodes(2:end) - dy/2;
-    dy = ones(1,n)*dy;
-end
